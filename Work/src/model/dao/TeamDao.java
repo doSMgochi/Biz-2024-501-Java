9edcf690e6d635724edb3773548295994e1a3129
doSMgochi <166393265+doSMgochi@users.noreply.github.com>
2024-07-03 14:23:23 +0900

    Hello 실습

---
+++ Work/src/model/dao/TeamDao.java
@@ -0,0 +1,32 @@
+package model.dao;
+
+import model.vo.Team;
+
+public class TeamDao {
+	public boolean addMember() {
+		try {
+			Team team = new Team();
+			// Database 에 멤버 추가
+
+			if (team.getTeamMemberA() == null) {
+				team.setTeamMemberA(null);
+
+				return true;
+			} else if (team.getTeamMemberB() == null) {
+				team.setTeamMemberB(null);
+
+				return true;
+			} else if (team.getTeamMemberC() == null) {
+				team.setTeamMemberC(null);
+
+				return true;
+			} else {
+				return false;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+}
